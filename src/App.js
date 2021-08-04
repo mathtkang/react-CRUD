a6868262058c705aa1c101c210d8861508777657
mathtkang <ksge1124@gmail.com>
2021-08-04 11:00:26 +0900

    var->const,let change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react' //react의 useState함수를 사용할거야
 function HeaderTag(props){
-  function onClickHeader(e){
+  const onClickHeader = (e) => {
     e.preventDefault();
     props.onChangeMode(); //이게 실행되면, onChangeModeHeader 함수가 실행된다
   }
@@ -27,13 +27,13 @@ function ReadTag(props){
 function NavTag(props){
   console.log('props.data', props.data);
 
-  function clickHander(e){
+  const clickHander = (e) => {
     e.preventDefault(); //a태그는 클릭하면 다음페이지로 넘어가는데, 넘어가지 않도록 막아주는 역할! (e:event의 객체)
     props.onChangeMode(Number(e.target.dataset.id)); //이게 실행되면, onChangeModeNav 함수가 실행된다
   }
 
-  var lis = [];
-  for(var i=0; i<props.data.length; i++){
+  let lis = [];
+  for(let i=0; i<props.data.length; i++){
     lis.push(<li><a data-id={props.data[i].id} onClick={clickHander}
       href={props.data[i].id+'.html'}>{props.data[i].title}</a></li>);
   }
@@ -53,44 +53,46 @@ function App() {
   const [mode, setMode] = useState('CREATE'); //이게 실행되면서, mode='WELCOME' 가 된다.(읽을 때는 앞에있는 mode로 읽고, 변경할 때는 setMode()함수로 변경)
   //mode의 디폴트 값을 CREATE 로 바꿔준다.
   const [id, setId] = useState(1);
-  var topics = [
+  let topics = [
     {id:1, title:'HTML', desc:'HTML is ...'},
     {id:2, title:'CSS', desc:'CSS is ...'}
   ];
 
-  function onChangeModeHeader(){
+  const onChangeModeHeader = () => {
     console.log('onChangeModeHeader');
     setMode('WELCOME');
   }
   // HeaderTag의 WEB을 클릭하면 onChangeMode에 입력한 함수가 실행되어 어떤 작업을 하게 하고 싶다.
-  function onChangeModeNav(id){
+  const onChangeModeNav = (id) => {
     console.log('onChangeModeNav', id);
     setMode('READ');
     // id값에 다른 UI를 변경하는 코드
     setId(id);
   }
 
-  function onChangeModeControl(_mode){
+  const onChangeModeControl = (_mode) => {
     console.log('onChangeModeControl', _mode);
     setMode(_mode);
   }
 
-  var article = null;
+  let article = null;
   if(mode === 'WELCOME'){
     article = <ReadTag title="Weclome" desc="Hello, WEB"></ReadTag>
   }else if(mode === 'READ'){
-    for(var i=0; i<topics.length; i++){
+    for(let i=0; i<topics.length; i++){
       if(topics[i].id === id){ //숫자 === 문자열 (false)
         article = <ReadTag title={topics[i].title} desc={topics[i].desc}></ReadTag>
         break;
       }
     }
   } else if(mode === 'CREATE'){
+    const onSubmitHandler = e => e.preventDefault();
     article = (
-      <form>
+      <form onSubmit={onSubmitHandler}>
+      {/* <form onSubmit={e.preventDefault()}> */}
         <h2>Create</h2>
-        <p><input type="text" placeholder="title"></input></p>
-        <p><textarea placeholder="description"></textarea></p>
+        <p><input name="title" type="text" placeholder="title"></input></p>
+        <p><textarea name="desc" placeholder="description"></textarea></p>
         <p><input type="submit"></input></p>
       </form>
     )
@@ -109,7 +111,7 @@ function App() {
 }
 
 function Control(props){
-  function clickHander(e){
+  const clickHander = (e) => {
     e.preventDefault();
     props.onChangeMode(e.target.dataset.mode);
   }
