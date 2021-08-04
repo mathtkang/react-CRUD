7a000b33d685f3315289a2da73f6c04f0e6f3f65
mathtkang <ksge1124@gmail.com>
2021-08-04 13:21:27 +0900

    create의 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -26,6 +26,25 @@ function ReadTag(props){
   )
 }
 
+function Create(props){
+  return (
+    <form onSubmit={e=>{
+      e.preventDefault();
+      var title = e.target.title.value;
+      var desc = e.target.desc.value;
+      props.onCreate({
+        title:title,
+        desc:desc
+      });
+    }}>
+      <h2>Create</h2>
+      <p><input name="title" type="text" placeholder="title"></input></p>
+      <p><textarea name="desc" placeholder="description"></textarea></p>
+      <p><input type="submit"></input></p>
+    </form>
+  )
+}
+
 function NavTag(props){
   console.log('props.data', props.data);
 
@@ -53,25 +72,20 @@ function NavTag(props){
 function App() {  
   console.log('App render');
   const [nextId, setNextId] = useState(3);
-  const [mode, setMode] = useState('CREATE'); //이게 실행되면서, mode='WELCOME' 가 된다.(읽을 때는 앞에있는 mode로 읽고, 변경할 때는 setMode()함수로 변경)
-  //mode의 디폴트 값을 CREATE 로 바꿔준다.
+  const [mode, setMode] = useState('CREATE'); 
   const [id, setId] = useState(1);
-  // let topics = [ //평범한 변수인 topics는 state가 아니기 때문에 다시 리로드 되지 않는다.
   let [topics, setTopics] = useState([
     {id:1, title:'HTML', desc:'HTML is ...'},
     {id:2, title:'CSS', desc:'CSS is ...'}
-    //article에 의해 받은 값을 여기에 넣어줌
   ]);
 
   const onChangeModeHeader = () => {
     console.log('onChangeModeHeader');
     setMode('WELCOME');
   }
-  // HeaderTag의 WEB을 클릭하면 onChangeMode에 입력한 함수가 실행되어 어떤 작업을 하게 하고 싶다.
   const onChangeModeNav = (id) => {
     console.log('onChangeModeNav', id);
     setMode('READ');
-    // id값에 다른 UI를 변경하는 코드
     setId(id);
   }
 
@@ -91,43 +105,18 @@ function App() {
       }
     }
   } else if(mode === 'CREATE'){
-    const onSubmitHandler = (e) => {
-      e.preventDefault();
-      const title = e.target.title.value;
-      const desc = e.target.desc.value;
-      // topics.push({ 
-      //   "id" : 3,
-      //   "title" : title,
-      //   "desc" : desc
-      // });
-      // setTopics(topics); //불변성, 가변성에 의해서 (복제후)아래와 같이 사용해줘야함
-
-      let newTopics = [...topics];
+    article = <Create onCreate={data=>{
+      var newTopics = [...topics];
       newTopics.push({
-        "id" : nextId,
-        "title" : title,
-        "desc" : desc
+        "id":nextId,
+        "title":data.title,
+        "desc":data.desc
       });
       setTopics(newTopics);
-      // 위의 newTopics 와 같은 방식
-      // setTopics([...topics,{
-      //   "id":3,
-      //   "title":title,
-      //   "desc":desc
-      // }]
       setMode('READ');
       setId(nextId);
       setNextId(nextId+1);
-
-    }
-    article = (
-      <form onSubmit={onSubmitHandler}>
-        <h2>Create</h2>
-        <p><input name="title" type="text" placeholder="title"></input></p>
-        <p><textarea name="desc" placeholder="description"></textarea></p>
-        <p><input type="submit"></input></p>
-      </form>
-    )
+    }}></Create>
   } else if(mode === 'UPDATE'){
     article = <div>Update</div>
   }
