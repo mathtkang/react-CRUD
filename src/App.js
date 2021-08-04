2c7d3b872db85bc0dbde9f61c8a02059aecb5744
mathtkang <ksge1124@gmail.com>
2021-08-04 13:44:25 +0900

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,30 @@ function ReadTag(props){
   )
 }
 
+function NavTag(props){
+  console.log('props.data', props.data);
+
+  const clickHander = (e) => {
+    e.preventDefault(); //a태그는 클릭하면 다음페이지로 넘어가는데, 넘어가지 않도록 막아주는 역할! (e:event의 객체)
+    props.onChangeMode(Number(e.target.dataset.id)); //이게 실행되면, onChangeModeNav 함수가 실행된다
+  }
+
+  let lis = [];
+  for(let i=0; i<props.data.length; i++){
+    lis.push(<li><a data-id={props.data[i].id} onClick={clickHander}
+      href={props.data[i].id+'.html'}>{props.data[i].title}</a></li>);
+  }
+
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+/////////
+
 function Create(props){
   return (
     <form onSubmit={e=>{
@@ -45,29 +69,27 @@ function Create(props){
   )
 }
 
-function NavTag(props){
-  console.log('props.data', props.data);
 
-  const clickHander = (e) => {
-    e.preventDefault(); //a태그는 클릭하면 다음페이지로 넘어가는데, 넘어가지 않도록 막아주는 역할! (e:event의 객체)
-    props.onChangeMode(Number(e.target.dataset.id)); //이게 실행되면, onChangeModeNav 함수가 실행된다
-  }
-
-  let lis = [];
-  for(let i=0; i<props.data.length; i++){
-    lis.push(<li><a data-id={props.data[i].id} onClick={clickHander}
-      href={props.data[i].id+'.html'}>{props.data[i].title}</a></li>);
-  }
-  //props.data.map(e) => <li>{e.title}</li>;
-
-  return (
-    <nav>
-      <ol>
-        {lis}
-      </ol>
-    </nav>
+function Update(props){
+  return(
+    <form onSubmit={e=>{
+      e.preventDefault();
+      var title = e.target.title.value;
+      var desc = e.target.desc.value;
+      props.onCreate({
+        title:title,
+        desc:desc
+      });
+    }}>
+      <h2>Update</h2>
+      <p><input name="title" type="text" placeholder="title" value={props.title}></input></p>
+      <p><textarea name="desc" placeholder="description" value={props.desc}></textarea></p>
+      <p><input type="submit"></input></p>
+    </form>
   )
 }
+
+///////////
 
 function App() {  
   console.log('App render');
@@ -88,7 +110,6 @@ function App() {
     setMode('READ');
     setId(id);
   }
-
   const onChangeModeControl = (_mode) => {
     console.log('onChangeModeControl', _mode);
     setMode(_mode);
@@ -99,7 +120,7 @@ function App() {
     article = <ReadTag title="Weclome" desc="Hello, WEB"></ReadTag>
   }else if(mode === 'READ'){
     for(let i=0; i<topics.length; i++){
-      if(topics[i].id === id){ //숫자 === 문자열 (false)
+      if(topics[i].id === id){
         article = <ReadTag title={topics[i].title} desc={topics[i].desc}></ReadTag>
         break;
       }
@@ -118,7 +139,27 @@ function App() {
       setNextId(nextId+1);
     }}></Create>
   } else if(mode === 'UPDATE'){
-    article = <div>Update</div>
+    for(let i=0; i<topics.length; i++){ //필터 이용하면 1줄로 요약 가능
+      if(topics[i].id === id){
+        article = <Update title={topics[i].title} desc={topics[i].desc} onUpdate={data=>{
+          //수정관련일
+          console.log('update', data);
+          var newTopics = [...topics];
+          for(let i=0; i<newTopics.length; i++){
+            if(newTopics[i].id === id){
+              newTopics[i] = {
+                "id" : newTopics[i].id,
+                "title":data.title,
+                "desc":data.desc
+              }
+            }
+          }
+          setTopics(newTopics);
+          setMode('READ');
+        }}></Update>
+        break;
+      }
+    }
   }
 
   return (
